9d1bba398c96471885f24b99188af6fb74b157e0
hyyqc <junjie_hyyqc@163.com>
2018-04-28 11:57:38 +0800

    路由过渡动画

--- vuerouter/src/App.vue
+++ vuerouter/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
-    <router-view name="left" style="width: 50%;float: left;height: auto;background: red;"/>
-    <router-view name="right" style="width: 50%;float: left;height: auto;background: green;"/>
+    <transition name="fade" mode="out-in"> 
+      <!-- mode 有两种模式 in-out out-in -->
+      <router-view/>
+      <router-view name="left" style="width: 50%;float: left;height: auto;background: red;"/>
+      <router-view name="right" style="width: 50%;float: left;height: auto;background: green;"/>
+    </transition>
     <br>
     <router-link to="/">Home</router-link>|
     <router-link to="/params/198/website">params</router-link>|
@@ -28,5 +31,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity .5s;
+}
+.fade-leave{
+  opacity: 1;
+}
+.fade-leave-active{
+  opacity: 0;
+  transition: opacity .5s
 }
 </style>
