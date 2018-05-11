dde8ffc6a086c4b95e753fc217dcd53ebdb17732
hyyqc <junjie_hyyqc@163.com>
2018-05-11 10:47:59 +0800

    订单结算

--- register/src/components/page/Pos.vue
+++ register/src/components/page/Pos.vue
@@ -5,19 +5,21 @@
   		<el-col :span='7' class="pos-order" id="order-list">
   			<el-tabs>
            <el-tab-pane label="点餐">
-
-              <el-table :data="tableData" border show-summary style="width:100%;overflow-x:hidden;">
+              <el-table :data="tableData" border style="width:100%;overflow-x:hidden;">
                 <el-table-column prop="goodsName" label="商品名称" align="center"></el-table-column>
                 <el-table-column prop="count" label="数量" align="center"></el-table-column>
                 <el-table-column prop="price" label="价格" align="center"></el-table-column>
                 <el-table-column label="操作" fixed="right" align="center">
-                  <template slot-scope="scope">
-                      <el-button type="text" size="small">增加</el-button>
+                  <template slot-scope="scope" class="operate">
                       <el-button type="text" size="small">删除</el-button>
+                      <el-button type="text" size="small" @click="addOrderList(scope.row)">增加</el-button>
                   </template>
                 </el-table-column>
               </el-table>
-
+              <div>
+                <span style="margin-right:25px">数量: {{totalCount}}</span>
+                <span>金额: {{totalMoney}}元</span>
+              </div>
               <el-row style="margin-top:20px">
                 <el-button type="warning" >挂单</el-button>
                 <el-button type="danger" >删除</el-button>
@@ -38,7 +40,7 @@
             <h2 class="universal-title">常用商品</h2>
             <div class="universal-items">
                  <ul>
-                   <li v-for="goods in universalGoods">
+                   <li v-for="goods in universalGoods" @click="addOrderList(goods)">
                      <span>{{goods.goodsName}}</span>
                      <span class="items-price">￥{{goods.price}}元</span>
                    </li>
@@ -48,7 +50,7 @@
             <el-tabs class="buttom-goods">
               <el-tab-pane label="主食">
                 <ul class="cook-items">
-                  <li v-for="cook in cookItems">
+                  <li v-for="cook in cookItems" @click="addOrderList(cook)">
                     <span class="foodImage"><img :src="cook.goodsImg"></span>
                     <span class="foodName">{{cook.goodsName}}</span>
                     <span class="foodPrice">￥{{cook.price}}元</span>
@@ -57,7 +59,7 @@
               </el-tab-pane>
               <el-tab-pane label="小食">
                 <ul class="cook-items">
-                  <li v-for="cook in cook1Items">
+                  <li v-for="cook in cook1Items" @click="addOrderList(cook)">
                     <span class="foodImage"><img :src="cook.goodsImg"></span>
                     <span class="foodName">{{cook.goodsName}}</span>
                     <span class="foodPrice">￥{{cook.price}}元</span>
@@ -66,7 +68,7 @@
               </el-tab-pane>
               <el-tab-pane label="饮料">
                 <ul class="cook-items">
-                  <li v-for="cook in cook2Items">
+                  <li v-for="cook in cook2Items" @click="addOrderList(cook)">
                     <span class="foodImage"><img :src="cook.goodsImg"></span>
                     <span class="foodName">{{cook.goodsName}}</span>
                     <span class="foodPrice">￥{{cook.price}}元</span>
@@ -75,7 +77,7 @@
               </el-tab-pane>
               <el-tab-pane label="套餐">
                 <ul class="cook-items">
-                  <li v-for="cook in cook3Items">
+                  <li v-for="cook in cook3Items" @click="addOrderList(cook)">
                     <span class="foodImage"><img :src="cook.goodsImg"></span>
                     <span class="foodName">{{cook.goodsName}}</span>
                     <span class="foodPrice">￥{{cook.price}}元</span>
@@ -97,23 +99,7 @@ export default {
   data(){
     return {
       // 点餐数据
-      tableData:[{
-          goodsName: '可口可乐',
-          price: 8,
-          count:1
-        }, {
-          goodsName: '香辣鸡腿堡',
-          price: 15,
-          count:1
-        }, {
-          goodsName: '爱心薯条',
-          price: 8,
-          count:1
-        }, {
-          goodsName: '甜筒',
-          price: 8,
-          count:1
-        }],
+      tableData:[],
       // 常用商品上
       universalGoods:[],
       // 常用商品下
@@ -121,9 +107,12 @@ export default {
       cook1Items:[],
       cook2Items:[],
       cook3Items:[],
+      totalMoney:0,
+      totalCount:0
     }
   },
   created(){
+    // created 生命周期钩子,当页面被创建,刷新时启用
     // 常用商品上
     axios.get('http://jspang.com/DemoApi/oftenGoods.php')
     .then(reponse => {
@@ -145,75 +134,42 @@ export default {
     .catch(error => {
       console.log(error);
     })
+  },
+  methods:{
+    addOrderList(goods){
+      // 初始化价格和数量
+      this.totalMoney = 0;
+      this.totalCount = 0;
+      // 判断数据有没有在左侧的列表中
+      let isHave = false;
+      for(let i=0;i<this.tableData.length;i++){
+        if(this.tableData[i].goodsId == goods.goodsId){
+          isHave = true;
+        }
+      }
+      if(isHave){
+        let arr = this.tableData.filter(obj => obj.goodsId == goods.goodsId);
+        arr[0].count++;
+      }else{
+        let newGoods = {
+          goodsId : goods.goodsId,
+          goodsName : goods.goodsName,
+          price : goods.price,
+          count : 1
+        };
+        this.tableData.push(newGoods);
+      }
+      // 计算价格和数量
+      this.tableData.forEach((element)=>{
+        this.totalCount += element.count;
+        this.totalMoney =this.totalMoney+(element.price * element.count);
+      })
+    }
   }
 }
 </script>
 
-<style lang="less" scoped>
-	.pos-order{
-		background-color: #F9FAFC;
-		border-right: 1px solid #C0CCDA;
-		height: 100vh;
-	}
-  .universal-goods{
-    .universal-title{
-      height:18px;
-      border-bottom: 2px solid #e4e7ed;
-      background-color: #F9FAFC;
-      margin-top: 0;
-      padding: 10px;
-      text-align: left;
-      font-weight: normal;
-      font:18px/18px "Microsoft YaHei";
-    }
-    .universal-items ul li{
-      list-style: none;
-      float: left;
-      border:1px solid #E5E9F2;
-      margin: 5px;
-      background-color: #fff;
-      padding: 10px;
-      .items-price{
-          color:#58B7FF;
-      }
-    }
-    .clear{
-      clear:both;
-    }
-    .buttom-goods{
-      margin-top: 20px;
-      .cook-items li{
-          list-style: none;
-          width:15%;
-          border:1px solid #E5E9F2;
-          height: auot;
-          overflow: hidden;
-          background-color:#fff;
-          padding: 2px;
-          float:left;
-          margin: 2px;
-          span{
-            display: block;
-            float:left;
-          }
-          .foodImage{
-            img{
-              width: 50%;
-            }
-         }
-         .foodName{
-             font-size: 18px;
-             padding-left: 10px;
-             color:brown;
-
-         }
-         .foodPrice{
-             font-size: 16px;
-             padding-left: 10px;
-         }
-      }
-    }
-  }
+<style lang="less" scoped src="../../assets/style.less">
 </style>
 
    <!-- mounted(){
