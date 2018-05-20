292d430f8129177d5d39a6f1611b7c8325811939
hyyqc <junjie_hyyqc@163.com>
2018-05-20 14:51:40 +0800

    some fix

--- vue-sell/src/components/HelloWorld.vue
+++ vue-sell/src/components/HelloWorld.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{author}}</h2>
+    <ul v-for="music in musics" :key="music.id">
+      <li>
+        {{music.name}}
+      </li>
+      <br>
+      <li>
+        <img :src="music.album.picUrl" style="width:200px;" alt="">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+const API_PROXY = 'https://bird.ioliu.cn/v1/?url='; // 远程代理
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: 'Welcome to Your Vue.js App',
+      author: 'hyyqc',
+      musics: []
+    };
+  },
+  mounted () {
+    let _this = this;
+    axios.get(API_PROXY + 'http://music.163.com/api/playlist/detail?id=19723756')
+      .then((res) => {
+        _this.musics = res.data.result.tracks;
+        console.log(res);
+      }, (error) => {
+        console.log(error);
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- vue-sell/src/main.js
+++ vue-sell/src/main.js
@@ -1,15 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import '../static/css/reset.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
-})
+});
