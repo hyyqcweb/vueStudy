e5b80d696977b610c4287a0e19e9d77cb2075ff5
hyyqc <junjie_hyyqc@163.com>
2018-06-01 17:13:50 +0800

    fix goods

--- vue-sell/src/components/goods/goods.vue
+++ vue-sell/src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
         <li v-for="item in goods" :key="item.id" class="menu-item">
           <span class="text border-1px">
@@ -10,25 +10,25 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:food-wrapper>
       <ul>
         <li v-for="item in goods" class="food-list" :key="item.id">
-          <h1>{{item.name}}</h1>
+          <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item" :key="food.id">
+            <li v-for="food in item.foods" class="food-item border-1px" :key="food.id">
               <div class="icon">
-                <img :src="food.icon" alt="">
+                <img width="57" height="57" :src="food.icon" alt="">
               </div>
               <div class="content">
-                <h2 class="name">{{food.name}}}</h2>
+                <h2 class="name">{{food.name}}</h2>
                 <p class="description">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>¥{{food.price}}</span>
-                  <span v-show="food.oldPrice">{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -40,34 +40,44 @@
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from 'axios';
+  import BScroll from 'better-scroll';
 
-const ERR_OK = 0;
+  const ERR_OK = 0;
 
-export default {
-  props: {
-    seller: {
-      type: Object
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      };
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
+      axios.get('/api/goods')
+        .then(res => {
+          res = res.data;
+          // console.log(res);
+          if (res.errno === ERR_OK) {
+            this.goods = res.data;
+            // console.log(res.data);
+            this.$nextTick(() => {
+              this._inisScroll();
+            });
+          }
+        });
+    },
+    methods: {
+      _inisScroll () {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {});
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {});
+      }
     }
-  },
-  data () {
-    return {
-      goods: []
-    };
-  },
-  created () {
-    this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
-    axios.get('/api/goods')
-      .then(res => {
-        res = res.data;
-        // console.log(res);
-        if (res.errno === ERR_OK) {
-          this.goods = res.data;
-          // console.log(res.data);
-        }
-    });
-  }
-};
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -90,11 +100,11 @@ export default {
         line-height: 14px
         padding: 0 12px // (80-56) / 2
         .text
-          display:table-cell
+          display: table-cell
           width: 56px
           vertical-align: middle
           font-size: 12px
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
           .icon
             display: inline-block
             width: 12px
@@ -115,4 +125,51 @@ export default {
               bg-image('./images/special_3')
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color:rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom:18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height:14px
+            font-size: 14px
+            line-height: 14px
+            color: rgb(7, 17, 27)
+          .description, .extra
+            font-size: 10px
+            line-height: 10px
+            color: rgb(147, 153, 159)
+          .description
+            margin-bottom: 8px
+          .extra
+            &.count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 </style>
